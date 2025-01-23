3b59614fe902c342a943e9ba13d3838c22a5aabd
Tekalig <tekahazi06@gmail.com>
2025-01-23 23:44:25 +0300

    feat: Integrate Sequelize database connection on server startup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const http = require('http');
-const path = require('path');
 const app = require('./src/index');
+const sequelize = require('./src/config/db');
 
 const server = http.createServer();
 
 server.on(app);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+    await sequelize.sync()
+    .then(() => console.log('Database connected'))
+    .catch((err) => console.log(err));
     console.log(`Server is running on port ${PORT}`);
 });
